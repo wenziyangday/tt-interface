6dc6c6f9ca2411067f564344a06629ece7d4fd0a
wenziyang <1483319587@qq.com>
2020-09-13 23:47:19 +0800

    add: 接下来要做的事情

--- utils/email_util.py
+++ utils/email_util.py
@@ -2,6 +2,7 @@
 
 
 # 发送邮件
+# TODO 发送邮件服务
 class EmailUtil:
     def __init__(self):
         pass
---
+++ utils/statistics_util.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+# TODO 表格统计用例执行成功和失败
+class StatisticUtil:
+    def __init__(self):
+        pass
