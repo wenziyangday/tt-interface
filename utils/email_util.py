f3e98e2b735dc581d908ecf56a82498eb17c0279
admin <wenziyangday@gmail.com>
2020-09-14 18:26:12 +0800

    update：邮箱发送邮件，含有附件的发送

--- utils/email_util.py
+++ utils/email_util.py
@@ -14,10 +14,14 @@ class EmailUtil:
 
     def send_mail(self):
         ret = True
+        # attachs = MIMEText(open('test.txt', 'rb').read(), 'base64', 'utf-8')
+        # attachs['Content-Type'] = 'application/octet-stream'
+        # attachs['Content-Disposition'] = 'attachment; filename="test.txt"'
         try:
-            msg = MIMEText('邮件内容', 'plain', 'utf-8')
-            msg['from'] = formataddr(['fromwen', self.sender])
-            msg['to'] = formataddr(['fk', self.receiver])
+            msg = MIMEText('这是衣蛾试试', 'plain', 'utf-8')
+            msg['from'] = formataddr(['张三', self.sender])
+            msg['to'] = formataddr(['李四', self.receiver])
+            # msg.attach(attachs)
             server = smtplib.SMTP_SSL('smtp.qq.com', 465)
             server.login(self.sender, self.password)
             server.sendmail(self.sender, [self.receiver, ], msg.as_string())
